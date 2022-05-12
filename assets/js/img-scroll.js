754d44fb661655e5f23a93ad23e0e54a96b72117
pcosin <74868663+pcosin@users.noreply.github.com>
2022-05-12 11:54:34 -0300

    antes de rellax.js

---
+++ assets/js/img-scroll.js
@@ -0,0 +1,18 @@
+// let aim = document.getElementById('image'), room;
+
+// window.addEventListener('load', setEdge);
+
+
+// window.addEventListener('scroll', function() {
+
+//   let ratio = (this.pageYOffset || this.scrollY)
+
+//   aim.style.setProperty('--epoch', ratio);
+// });
+
+// function setEdge() {
+
+//   room = window.innerHeight;
+
+//   aim.style.setProperty('--maximum', room-aim.height + 'px');
+// }
