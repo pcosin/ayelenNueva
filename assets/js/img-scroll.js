377b5a09ea7c95855e02e8f7a68368ebf6c173d5
pcosin <74868663+pcosin@users.noreply.github.com>
2022-05-12 20:09:25 -0300

    falta arreglar fixed img

--- assets/js/img-scroll.js
+++ assets/js/img-scroll.js
@@ -16,3 +16,32 @@
 
 //   aim.style.setProperty('--maximum', room-aim.height + 'px');
 // }
+
+let rellax = new Rellax('.rellax', {
+    wrapper: '.hero-inside'
+  });
+
+
+var socialFloat = document.querySelector('.rellax');
+var footer = document.querySelector('#before-footer');
+
+function checkOffset() {
+  function getRectTop(el){
+    var rect = el.getBoundingClientRect();
+    return rect.top;
+  }
+  
+  if((getRectTop(socialFloat) + document.body.scrollTop) + socialFloat.offsetHeight >= (getRectTop(footer) + document.body.scrollTop) - 10)
+    socialFloat.style.bottom = '350px';
+  if(document.body.scrollTop + window.innerHeight < (getRectTop(footer) + document.body.scrollTop))
+    socialFloat.style.bottom = '0px'; // restore when you scroll up
+  
+  socialFloat.innerHTML = document.body.scrollTop + window.innerHeight;
+}
+
+document.addEventListener("scroll", function(){
+  checkOffset();
+});
+
+
+
