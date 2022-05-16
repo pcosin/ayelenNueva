c4b8602fc1e738583093890c07b5dbe41ccd817e
pcosin <74868663+pcosin@users.noreply.github.com>
2022-05-16 12:39:48 -0300

    falta contacto

--- assets/js/img-scroll.js
+++ assets/js/img-scroll.js
@@ -31,10 +31,11 @@ function checkOffset() {
     return rect.top;
   }
   
-  if((getRectTop(socialFloat) + document.body.scrollTop) + socialFloat.offsetHeight >= (getRectTop(footer) + document.body.scrollTop) - 10)
-    socialFloat.style.bottom = '350px';
+  if((getRectTop(socialFloat) + document.body.scrollTop) + socialFloat.offsetHeight >= (getRectTop(footer) + document.body.scrollTop) - 100)
+    socialFloat.style.position = 'absolute';
+    socialFloat.classList.add('position')
   if(document.body.scrollTop + window.innerHeight < (getRectTop(footer) + document.body.scrollTop))
-    socialFloat.style.bottom = '0px'; // restore when you scroll up
+    socialFloat.style.position = 'fixed'; // restore when you scroll up
   
   socialFloat.innerHTML = document.body.scrollTop + window.innerHeight;
 }
@@ -44,4 +45,37 @@ document.addEventListener("scroll", function(){
 });
 
 
+
+
+
+// function checkOffset() {
+//     var a=$(document).scrollTop()+window.innerHeight;
+//     var b=$('#section').offset().top;
+//     if (a<b) {
+//       $('#image').css('bottom', '100px');
+//     } else {
+//       $('#image').css('bottom', (500+(a-b))+'px');
+//     }
+//   }
+//   $(document).ready(checkOffset);
+//   $(document).scroll(checkOffset);
+
+
+
+//   $(window).scroll(() => {
+//     //Distance from top fo document to top of footer
+//     topOfFooter = $('#footer').position().top;
+//      // Distance user has scrolled from top + windows inner height
+//      scrollDistanceFromTopOfDoc = $(document).scrollTop() + window.innerHeight;
+//      // Difference between the two.
+//      scrollDistanceFromTopOfFooter = scrollDistanceFromTopOfDoc - topOfFooter; 
+//     // If user has scrolled further than footer,
+//       if (scrollDistanceFromTopOfDoc > topOfFooter) {
+//         // add margin-bottom so button stays above footer.
+//         $('#floating-button').css('margin-bottom',  0 + scrollDistanceFromTopOfFooter);
+//       } else  {
+//         // remove margin-bottom so button goes back to the bottom of the page
+//         $('#floating-button').css('margin-bottom', 0);
+//       }
+//     });
 
