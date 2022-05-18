f98f081d6761e1fbad258624b3bc99250a9fbf70
pcosin <74868663+pcosin@users.noreply.github.com>
2022-05-18 16:52:07 -0300

    probando preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,3 +88,11 @@ window.onload = fadeIn;
             }, 250);
         }
 
+
+        // Preloader
+ window.addEventListener("load", () => {
+    document.querySelector(".preloader").classList.add("fade-out");
+    setTimeout(() => {
+        document.querySelector(".preloader").style.display = "none";
+    }, 600)
+})
