b19740524c65a4528ac5c2ce0268505cb3dd490c
pcosin <74868663+pcosin@users.noreply.github.com>
2022-05-22 09:07:59 -0300

    final

--- assets/js/img-scroll.js
+++ assets/js/img-scroll.js
@@ -1,28 +1,10 @@
-// let aim = document.getElementById('image'), room;
-
-// window.addEventListener('load', setEdge);
-
-
-// window.addEventListener('scroll', function() {
-
-//   let ratio = (this.pageYOffset || this.scrollY)
-
-//   aim.style.setProperty('--epoch', ratio);
-// });
-
-// function setEdge() {
-
-//   room = window.innerHeight;
-
-//   aim.style.setProperty('--maximum', room-aim.height + 'px');
-// }
 
 let rellax = new Rellax('.rellax', {
     wrapper: '.hero-inside'
   });
 
 
-let socialFloat = document.querySelector('.rellax');
+let socialFloat = document.querySelector('#image');
 let footer = document.querySelector('#before-footer');
 
 function checkOffset() {
@@ -31,14 +13,17 @@ function checkOffset() {
     return rect.top;
   }
   
-  if((getRectTop(socialFloat) + document.body.scrollTop) + socialFloat.offsetHeight >= (getRectTop(footer) + document.body.scrollTop) - 100)
+  if((getRectTop(socialFloat) + document.body.scrollTop) + socialFloat.offsetHeight >= (getRectTop(footer) + document.body.scrollTop) - 100){
     socialFloat.style.position = 'absolute';
-    socialFloat.style.bottom = '100px';
-
+    socialFloat.style.top = '1810px';
+   
+  }
   if(document.body.scrollTop + window.innerHeight < (getRectTop(footer) + document.body.scrollTop))
-    socialFloat.style.position = 'fixed'; // restore when you scroll up
+   { socialFloat.style.position = 'fixed'; // restore when you scroll up
+    socialFloat.style.top = '15%';
+   
   
-  socialFloat.innerHTML = document.body.scrollTop + window.innerHeight;
+  }  socialFloat.innerHTML = document.body.scrollTop + window.innerHeight;
 }
 
 document.addEventListener("scroll", function(){
@@ -49,21 +34,4 @@ document.addEventListener("scroll", function(){
 
 
 
-
-//   $(window).scroll(() => {
-//     //Distance from top fo document to top of footer
-//     topOfFooter = $('#footer').position().top;
-//      // Distance user has scrolled from top + windows inner height
-//      scrollDistanceFromTopOfDoc = $(document).scrollTop() + window.innerHeight;
-//      // Difference between the two.
-//      scrollDistanceFromTopOfFooter = scrollDistanceFromTopOfDoc - topOfFooter; 
-//     // If user has scrolled further than footer,
-//       if (scrollDistanceFromTopOfDoc > topOfFooter) {
-//         // add margin-bottom so button stays above footer.
-//         $('#floating-button').css('margin-bottom',  0 + scrollDistanceFromTopOfFooter);
-//       } else  {
-//         // remove margin-bottom so button goes back to the bottom of the page
-//         $('#floating-button').css('margin-bottom', 0);
-//       }
-//     });
 
