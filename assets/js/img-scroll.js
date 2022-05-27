de86aa32824a6c725469d7ac5fd63dcdcc70293d
pcosin <74868663+pcosin@users.noreply.github.com>
2022-05-27 15:14:47 -0300

    fixed img

--- assets/js/img-scroll.js
+++ assets/js/img-scroll.js
@@ -1,37 +1,30 @@
+let rellax = new Rellax(".rellax", {
+  wrapper: ".hero-inside",
+});
 
-let rellax = new Rellax('.rellax', {
-    wrapper: '.hero-inside'
-  });
-
-
-let socialFloat = document.querySelector('#image');
-let footer = document.querySelector('#before-footer');
+let socialFloat = document.querySelector("#image");
+let footer = document.querySelector("#before-footer");
 
 function checkOffset() {
-  function getRectTop(el){
+  function getRectTop(el) {
     let rect = el.getBoundingClientRect();
     return rect.top;
   }
-  
-  if((getRectTop(socialFloat) + document.body.scrollTop) + socialFloat.offsetHeight >= (getRectTop(footer) + document.body.scrollTop) - 100){
-    socialFloat.style.position = 'absolute';
-    socialFloat.style.top = '1810px';
-   
+
+  if (
+    getRectTop(socialFloat) + document.body.scrollTop + socialFloat.offsetHeight >=
+    getRectTop(footer) + document.body.scrollTop - 100
+  ) {
+    socialFloat.style.position = "absolute";
+    socialFloat.style.top = "1910px";
   }
-  if(document.body.scrollTop + window.innerHeight < (getRectTop(footer) + document.body.scrollTop))
-   { socialFloat.style.position = 'fixed'; // restore when you scroll up
-    socialFloat.style.top = '15%';
-   
-  
-  }  socialFloat.innerHTML = document.body.scrollTop + window.innerHeight;
+  if (document.body.scrollTop + window.innerHeight < getRectTop(footer) + document.body.scrollTop) {
+    socialFloat.style.position = "fixed"; // restore when you scroll up
+    socialFloat.style.top = "15%";
+  }
+  socialFloat.innerHTML = document.body.scrollTop + window.innerHeight;
 }
 
-document.addEventListener("scroll", function(){
+document.addEventListener("scroll", function () {
   checkOffset();
 });
-
-
-
-
-
-
